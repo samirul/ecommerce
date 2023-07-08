95b320d3b6e2d944b5491f110208df4f3e9d8b8f
samirul <samirulhaque08@gmail.com>
2023-07-08 20:43:30 +0530

    Added/Updated some of html files in django

--- base/views.py
+++ base/views.py
@@ -3,4 +3,12 @@ from django.shortcuts import render
 # Create your views here.
 
 def home(request):
-    return render(request, "index.html")
+    return render(request, "base/index.html")
+
+
+def register(request):
+    return render(request, "base/register.html")
+
+
+def login(request):
+    return render(request, "base/login.html")
--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -87,6 +87,14 @@ USE_TZ = True
 
 STATIC_URL = "static/"
 
+MEDIA_URL = '/images/'
+
+STATICFILES_DIRS = [
+    BASE_DIR / 'static'
+]
+
+MEDIA_ROOT = BASE_DIR / 'static/images'
+
 
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
