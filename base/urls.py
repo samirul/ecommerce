8b0661659fe835fae4ef6627200424b316cdf473
samirul <samirulhaque08@gmail.com>
2023-09-17 22:53:26 +0530

    removed slug in base app

--- base/urls.py
+++ base/urls.py
@@ -3,11 +3,9 @@ from . import views
 
 urlpatterns = [
     path("", views.HomeView.as_view(), name='home'),
-    # path("items/products/<slug>/", views.HomeView.as_view(), name='slughome'),
     path("contact-us/", views.ContactUsView.as_view(), name='contact-us'),
     path("about/", views.AboutView.as_view(), name='about'),
     path("services/", views.ServiceView.as_view(), name='services'),
-    # path("<slugcategory>/", views.CategorySlugLink.as_view(), name="categorysluglink")
-
+    
 
 ]
--- base/views.py
+++ base/views.py
@@ -3,16 +3,6 @@ from django.views import View
 from products.models import Categories
 from .models import HomeSlider, HomeMiddleBanner
 
-# class CategorySlugLink(View):
-#     def get(self, request, slugcategory):
-#         categories = Categories.objects.get(category_link=slugcategory)
-#         context={
-#             "categories_slug" : categories
-#         }
-#         return render(request, "base/index.html", context=context)
-
-
-
 class HomeView(View):
     def get(self, request):
          categories = Categories.objects.prefetch_related('sub_categories').all()
@@ -24,14 +14,6 @@ class HomeView(View):
             "homemiddlebanner" : homemiddlebanner
             }
          return render(request, "base/index.html", context=context)
-    
-    # def get(self, request, slug):
-    #     categories_slug = Categories.objects.get(category_link=slug)
-    #     context = {
-    #         "categories_slug" : categories_slug,
-    #         }
-    #     return render(request, "base/index.html", context=context)
-    
 
 class ContactUsView(View):
     def get(self, request):
