d847f5d892d9f3906be03d9b11c3f27fe9226861
Samirul Haque <samirulhaque08@gmail.com>
2024-02-14 18:52:29 +0900

    modified static_root and staticfiles_dirs in settings

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -92,15 +92,14 @@ USE_I18N = True
 USE_TZ = True
 
 
-STATIC_URL = "/static/"
+STATIC_URL = "static/"
 
-STATIC_ROOT = "staticfiles_build"
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build', 'static')
 
 MEDIA_URL = "/images/"
 
-STATICFILES_DIRS = [
-    BASE_DIR / "static",
-]
+STATICFILES_DIRS = os.path.join(BASE_DIR, 'static'),
+
 
 MEDIA_ROOT = BASE_DIR / "static/images"
 
