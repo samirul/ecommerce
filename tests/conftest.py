81471d0285a80e024536bfe4502f8359e0d261c5
Samirul Haque <samirulhaque08@gmail.com>
2024-06-12 17:21:40 +0900

    test all product views print response context

--- tests/conftest.py
+++ tests/conftest.py
@@ -181,10 +181,10 @@ def register_user_for_login_test_fail_for_is_active_false(client, registration_u
 
 @pytest.fixture
 def create_categories_and_subcategories():
-    category, description, subcategory = "Books", "This is nice text book", "Books"
-    Category.objects.create(category_name=category, category_description=description)
-    Subcategory.objects.create(subcategory_name=subcategory, categories=category)
-    return category, description, subcategory
+    category_name, description, subcategory_name = "Books", "This is nice text book", "Books"
+    category = Category.objects.create(category_name=category_name, category_description=description)
+    Subcategory.objects.create(subcategory_name=subcategory_name, categories=category.id)
+    return category, description, subcategory_name
 
 #...............................................................
 # Ended Products
