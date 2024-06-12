c5c81fb70616dca4035840ee405ed50e332cfd06
Samirul Haque <samirulhaque08@gmail.com>
2024-06-12 18:10:02 +0900

    check all categories length, check all categories, category_name match

--- tests/test_all_product_views.py
+++ tests/test_all_product_views.py
@@ -10,5 +10,5 @@ def test_view_all_products(client, create_categories_and_subcategories):
     cate_name = [category for category in response.context['categories']]
     assert len(response.context['categories']) == 1
     assert list(response.context['categories']) == list(Category.objects.all())
-    assert cate_name == category_name
+    assert cate_name == [category_name]
 
