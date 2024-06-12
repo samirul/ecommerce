a43261e16ce6fa17bdc3e6f5843ef0a5dd70697f
Samirul Haque <samirulhaque08@gmail.com>
2024-06-12 15:37:42 +0900

    test all product views print response

---
+++ tests/test_all_product_views.py
@@ -0,0 +1,9 @@
+from django.urls import reverse
+import pytest
+
+@pytest.mark.django_db()
+def test_view_all_products(client):
+    response = client.get(reverse('all-products'))
+    print(response)
+
+
