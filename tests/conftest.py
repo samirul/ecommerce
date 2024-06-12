ce7f564684f4d124d071d7bfa8203eee4e08f851
Samirul Haque <samirulhaque08@gmail.com>
2024-06-12 17:15:36 +0900

    test all product views print response context 6

--- tests/conftest.py
+++ tests/conftest.py
@@ -181,10 +181,10 @@ def register_user_for_login_test_fail_for_is_active_false(client, registration_u
 
 @pytest.fixture
 def create_categories_and_subcategories():
-    category, description, subcategory, categories_id = "Books", "This is nice text book", "Books", 1
+    category, description, subcategory = "Books", "This is nice text book", "Books"
     Category.objects.create(category_name=category, category_description=description)
-    Subcategory.objects.create(subcategory_name=subcategory, categories=categories_id)
-    return category, description, subcategory, categories_id
+    Subcategory.objects.create(subcategory_name=subcategory, categories=category)
+    return category, description, subcategory
 
 #...............................................................
 # Ended Products
--- tests/test_all_product_views.py
+++ tests/test_all_product_views.py
@@ -3,7 +3,7 @@ import pytest
 
 @pytest.mark.django_db()
 def test_view_all_products(client, create_categories_and_subcategories):
-    category, description, subcategory, categories_id = create_categories_and_subcategories
+    category, description, subcategory = create_categories_and_subcategories
     response = client.get(reverse('all-products'))
     assert response.context['categories'].category_name == category
 
