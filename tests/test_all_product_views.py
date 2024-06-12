1d49c1e841a1a39799a39c46c9f361345a73b480
Samirul Haque <samirulhaque08@gmail.com>
2024-06-12 17:33:26 +0900

    test all product views print response context

--- tests/test_all_product_views.py
+++ tests/test_all_product_views.py
@@ -3,8 +3,8 @@ import pytest
 
 @pytest.mark.django_db()
 def test_view_all_products(client, create_categories_and_subcategories):
-    category, description, subcategory = create_categories_and_subcategories
+    category_name, description, subcategory_name = create_categories_and_subcategories
     response = client.get(reverse('all-products'))
-    assert response.context['categories'].category_name == category
+    assert len(response.context['categories']) == 1
 
 
