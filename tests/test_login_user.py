f36b00a31ad7c6eade327c9e5adf5a58796cbe8d
Samirul Haque <samirulhaque08@gmail.com>
2024-06-12 14:22:29 +0900

    test login user fail for no email found in the database

--- tests/test_login_user.py
+++ tests/test_login_user.py
@@ -19,7 +19,7 @@ def test_login_user(client, register_user_for_login_test_pass_for_is_active_true
 
 
 @pytest.mark.django_db()
-def test_login_user_fail(client, register_user_for_login_test_fail_for_is_active_false):
+def test_login_user_fail_for_is_active_false(client, register_user_for_login_test_fail_for_is_active_false):
     email, password = register_user_for_login_test_fail_for_is_active_false
 
     response = client.post(reverse("login"),{
@@ -29,4 +29,15 @@ def test_login_user_fail(client, register_user_for_login_test_fail_for_is_active
 
     messages = list(get_messages(response.wsgi_request))
     assert any(f"{email} - Email isn't Verified, Please Check Your Email And Verify Your Account First." in str(message) for message in messages)
+    assert response.status_code == 302
+
+@pytest.mark.django_db()
+def test_login_failed_for_no_email_found_in_the_database(client):
+    response = client.post(reverse("login"),{
+        "email": "catdog4201@gmail.com",
+        "password": "catdog1@!4H"
+    })
+
+    messages = list(get_messages(response.wsgi_request))
+    assert any("catdog420@gmail.com - Email isn't Registered, Please Register Your Account First." in str(message) for message in messages)
     assert response.status_code == 302
